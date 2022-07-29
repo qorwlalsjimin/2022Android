cbb5901422ca43a7b27f59b59e4ae9e420155bc4
지민 백 <qorwlalsjimin@naver.com>
2022-07-29 12:26:00 +0900

    사칙연산 라디오 버튼으로 추가

--- app/src/main/java/kr/hs/s2104/mirim_project_0729_1/MainActivity.java
+++ app/src/main/java/kr/hs/s2104/mirim_project_0729_1/MainActivity.java
@@ -8,13 +8,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText edit1, edit2;
-    Button btnPlus;
+    Button btnCalc;
+    RadioGroup radio;
 
+    final static int PLUS = 0;
+    final static int MINUS = 1;
+    final static int MULTI = 2;
+    final static int DIVIDE = 3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,14 +29,32 @@ public class MainActivity extends AppCompatActivity {
 
         edit1 = findViewById(R.id.edit1);
         edit2 = findViewById(R.id.edit2);
-        btnPlus = findViewById(R.id.btn_plus);
+        btnCalc = findViewById(R.id.btn_calc);
+        radio = findViewById(R.id.radio_oper);
 
-        btnPlus.setOnClickListener(new View.OnClickListener() {
+        btnCalc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
                 intent.putExtra("edit1", Integer.parseInt(edit1.getText().toString()));
                 intent.putExtra("edit2", Integer.parseInt(edit2.getText().toString()));
+
+                int oper = 0;
+                switch (radio.getCheckedRadioButtonId()){
+                    case R.id.radio_plus:
+                       oper = PLUS;
+                       break;
+                    case R.id.radio_minus:
+                        oper = MINUS;
+                        break;
+                    case R.id.radio_multi:
+                        oper = MULTI;
+                        break;
+                    case R.id.radio_divide:
+                        oper = DIVIDE;
+                        break;
+                }
+                intent.putExtra("oper", oper);
                 startActivityForResult(intent, 0);
             }
         });
@@ -39,8 +64,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK){
-            int sum = data.getIntExtra("sum", 0);
-            Toast.makeText(getApplicationContext(), "덧셈결과"+sum, Toast.LENGTH_LONG).show();
+            int result = data.getIntExtra("result", 0);
+            Toast.makeText(getApplicationContext(), "결과 :"+result, Toast.LENGTH_LONG).show();
         }
     }
 }
--- app/src/main/java/kr/hs/s2104/mirim_project_0729_1/SecondActivity.java
+++ app/src/main/java/kr/hs/s2104/mirim_project_0729_1/SecondActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioGroup;
 
 public class SecondActivity extends AppCompatActivity {
 
-    int sum;
+    int result;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,18 +19,34 @@ public class SecondActivity extends AppCompatActivity {
         Intent intent = getIntent();
         int num1 = intent.getIntExtra("edit1", 0);
         int num2 = intent.getIntExtra("edit2",0);
+        int oper = intent.getIntExtra("oper",0);
 
-        sum = num1 + num2;
+        switch (oper){
+            case MainActivity.PLUS:
+                result = num1+num2;
+                break;
+            case MainActivity.MINUS:
+                result = num1-num2;
+                break;
+            case MainActivity.MULTI:
+                result = num1*num2;
+                break;
+            case MainActivity.DIVIDE:
+                result = num1/num2;
+                break;
+        }
         Button btnBack = findViewById(R.id.btn_back);
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                intent.putExtra("sum", sum);
+                intent.putExtra("result", result);
                 setResult(RESULT_OK, intent);
                 finish();
             }
         });
+        
+        
 
     }
 }
