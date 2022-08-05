e3233a41419ec71d1b4ed44598d2f5a872e818aa
지민 백 <qorwlalsjimin@naver.com>
2022-08-05 11:48:11 +0900

    구글맵 사용 예제

---
+++ app/src/main/java/kr/hs/s2104/mirim_project_0805_googlemap/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.hs.s2104.mirim_project_0805_googlemap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
