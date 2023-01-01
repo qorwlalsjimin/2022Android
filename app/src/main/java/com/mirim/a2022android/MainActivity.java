a8071d49b1f7b0ad79d84257fa1359e263d53a99
지민 백 <s2104@e-mirim.hs.kr>
2023-01-01 19:33:53 +0900

    Initial commit

---
+++ app/src/main/java/com/mirim/a2022android/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mirim.a2022android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
